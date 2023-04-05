7b5f213bad73fe0f45ca5b0f2a2cf6dfb7a5013c
Mohammad Asad Mohammad <mmohammadq8@outlook.com>
2023-04-05 23:59:32 +0300

    header done

--- themes/momosa/assets/js/main.js
+++ themes/momosa/assets/js/main.js
@@ -102,3 +102,24 @@ window.addEventListener('resize', updateStyles)
 const myModal = new HystModal({
   linkAttributeName: "data-hystmodal",
 });
+
+function disableTransitionsTemporarily() {
+  document.documentElement.classList.add('[&_*]:!transition-none')
+  window.setTimeout(() => {
+    document.documentElement.classList.remove('[&_*]:!transition-none')
+  }, 0)
+}
+
+function toggleMode() {
+  disableTransitionsTemporarily()
+
+  let darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)')
+  let isSystemDarkMode = darkModeMediaQuery.matches
+  let isDarkMode = document.documentElement.classList.toggle('dark')
+
+  if (isDarkMode === isSystemDarkMode) {
+    delete window.localStorage.isDarkMode
+  } else {
+    window.localStorage.isDarkMode = isDarkMode
+  }
+}
