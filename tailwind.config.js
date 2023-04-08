f16c0c035d718ad7d82587b60d7c1df02bdaa6c6
Mohammad Asad Mohammad <mmohammadq8@outlook.com>
2023-04-09 00:17:01 +0300

    updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -57,7 +57,7 @@ module.exports = {
           '--tw-prose-bullets': theme('colors.zinc.900'),
           '--tw-prose-hr': theme('colors.zinc.100'),
           '--tw-prose-quote-borders': theme('colors.zinc.200'),
-          '--tw-prose-captions': theme('colors.zinc.400'),
+          '--tw-prose-captions': theme('colors.zinc.300'),
           '--tw-prose-code': theme('colors.zinc.700'),
           '--tw-prose-code-bg': theme('colors.zinc.300 / 0.2'),
           '--tw-prose-pre-code': theme('colors.zinc.100'),
@@ -66,7 +66,7 @@ module.exports = {
           '--tw-prose-th-borders': theme('colors.zinc.200'),
           '--tw-prose-td-borders': theme('colors.zinc.100'),
 
-          '--tw-prose-invert-body': theme('colors.zinc.400'),
+          '--tw-prose-invert-body': theme('colors.zinc.300'),
           '--tw-prose-invert-headings': theme('colors.zinc.200'),
           '--tw-prose-invert-links': theme('colors.teal.400'),
           '--tw-prose-invert-links-hover': theme('colors.teal.400'),
